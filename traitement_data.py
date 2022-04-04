eda15f0c7e4cad7a9e8637bfbdd7fb56acbef36d
de Haut de Sigy Thomas <thomas.de-haut-de-sigy@student-cs.fr>
2022-04-04 16:57:10 +0200

    filtre pb

--- traitement_data.py
+++ traitement_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-pas = 1000
+pas = 500
 result = []
 tab = np.loadtxt("data.txt", dtype="int")
 for i in range(len(tab) - pas):
@@ -10,5 +10,23 @@ for i in range(len(tab) - pas):
         result[i] += tab[i + j]
 # print(len(tab_sum))
 
-plt.plot(result)
+# plt.plot(result)
+# plt.show()
+
+# Fréquence de coupure
+fc = 0.22  # Hz
+tau = 1/(2*np.pi*fc)
+
+# Période d'échantillonnage
+Te = 1  # s
+
+s_pb = []
+s_pb.append(result[0])
+
+# Filtrage
+for i in range(1, len(result)):
+    s_pb.append(s_pb[i-1]+Te/tau*(result[i-1]-s_pb[i-1]))
+
+plt.plot(result, color='silver', label='Signal')
+plt.plot(s_pb, color='#cc0000', label='Signal filtré')
 plt.show()
