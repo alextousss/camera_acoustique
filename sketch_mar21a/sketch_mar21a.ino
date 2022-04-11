15b2374e0d236c6fe1cf632e95077a84bec6e9ad
de Haut de Sigy Thomas <thomas.de-haut-de-sigy@student-cs.fr>
2022-04-11 14:24:06 +0200

    suppression

--- sketch_mar21a/sketch_mar21a.ino
+++
@@ -1,3 +0,0 @@
-// Create PDM receiver object, with Clock and Data pins used (not all pins available)
-Adafruit_ZeroPDM pdm = Adafruit_ZeroPDM(1, 4);  // Metro M0 or Arduino zero
-//Adafruit_ZeroPDM pdm = Adafruit_ZeroPDM(25, 26);  // CPlay express
